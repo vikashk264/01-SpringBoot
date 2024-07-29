b85ae5a02c478c00dd0db1aed6d6820a147163d7
VIKASH <vikashviru194@gmail.com>
2024-07-29 16:53:16 +0530

    method added

--- src/main/java/com/app/entity/Hello.java
+++ src/main/java/com/app/entity/Hello.java
@@ -3,6 +3,10 @@ package com.app.entity;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to github tutorial by Er.Vikash");
+		System.out.println("Welcome");
+	}
+	
+	public void get(){
+		System.out.println("Print Something")
 	}
 }
