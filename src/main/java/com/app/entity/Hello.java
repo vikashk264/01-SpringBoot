3ff303d58da0e1b6740eeb03968477b40adc76e8
VIKASH <vikashviru194@gmail.com>
2024-07-30 11:31:13 +0530

    HIS-300: Related changes

--- src/main/java/com/app/entity/Hello.java
+++ src/main/java/com/app/entity/Hello.java
@@ -1,6 +1,7 @@
 package com.app.entity;
 
 public class Hello {
+	private static final String REST_URL="https:"//www.google.com";
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to github tutorial by Er.Vikash");
